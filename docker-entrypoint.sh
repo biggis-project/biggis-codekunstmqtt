b2e58eb3371c99c3f7f6282df8cd42cbd8658563
Jochen Lutz <jlu@gmx.de>
2017-07-03 15:51:16 +0200

    Added Kafka server as parameter

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 #java -Dsbsrs.kafkaServer="$KAFKA_SERVER" -jar /opt/sbsrs/sbsrs.jar
 
-java -Dmqtt.brokerUrl=$CODEKUNST_SERVER -Dmqtt.topic=$CODEKUNST_TOPIC -jar CodekunstMQTTAdapter-assembly-$CODEKUNSTMQTT_VERSION.jar
+java -Dmqtt.brokerUrl=$CODEKUNST_SERVER -Dmqtt.topic=$CODEKUNST_TOPIC -jar CodekunstMQTTAdapter-assembly-$CODEKUNSTMQTT_VERSION.jar -Dkafka.server=$KAFKA_SERVER
