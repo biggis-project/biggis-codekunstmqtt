ff72aa440306b72ef3e3b58b51b5e4b16a9a135a
Jochen Lutz <jlu@gmx.de>
2017-07-27 11:10:25 +0200

    Updated to v3.1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,4 +12,4 @@ fi
 
 echo "More MQTT: $MORE_MQTT_OPTIONS"
 
-java -Dmqtt.brokerUrl=$CODEKUNST_SERVER -Dmqtt.topic=$CODEKUNST_TOPIC $MORE_MQTT_OPTIONS -Dkafka.server=$KAFKA_SERVER -jar CodekunstMQTTAdapter-assembly-$CODEKUNSTMQTT_VERSION.jar
+java -Dmqtt.brokerUrl=$CODEKUNST_SERVER -Dmqtt.topic=$CODEKUNST_TOPIC -Dmqtt.unifiedTopic=$CODEKUNST_UNIFIED_TOPIC $MORE_MQTT_OPTIONS -Dkafka.server=$KAFKA_SERVER -jar CodekunstMQTTAdapter-assembly-$CODEKUNSTMQTT_VERSION.jar
